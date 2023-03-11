74da3bd75ed17c34a6ac0d894df6e8f6b1eae61b
A-D-7 <115519144+A-D-7@users.noreply.github.com>
2023-03-11 17:47:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var rand1 = Math.floor(Math.random()*6)+1;
 var rand2 = Math.floor(Math.random()*6)+1;
 
-var im1 = document.querySelectorAll("img")[0].src = "images/dice" + rand1 +".png";
-var im2 = document.querySelectorAll("img")[1].src = "images/dice" + rand2 +".png";
+var im1 = document.querySelectorAll("img")[0].src = "dice" + rand1 +".png";
+var im2 = document.querySelectorAll("img")[1].src = "dice" + rand2 +".png";
 
 if(rand1>rand2){
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
